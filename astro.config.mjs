0e3d9c42378504e5a8613f1612afdeaec207a902
Patrick McCormack <mccpair@MacBookAir.fritz.box>
2024-11-16 19:05:38 +0100

    Update configuration for static rendering on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',  // Changed from 'server' to 'static' for GitHub Pages
   integrations: [tailwind()],
   site: 'https://rdmaal.github.io',
   base: '/WTrendspotter',
